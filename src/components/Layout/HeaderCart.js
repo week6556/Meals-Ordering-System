73202e02fec7084bec874b3c478336508e7c508f
Wei Fan <fanwei6556@gmail.com>
2021-07-24 00:13:53 +1000

    add items and remove items are avaliable to use

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,14 +1,20 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCart.module.css";
+import CartContext from "../../store/cart-context";
+import { useContext } from "react";
 
 const HeaderCart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 };
