5ebc583d7ae00407f414c09b5b28bf1b9f174eb9
Wei Fan <fanwei6556@gmail.com>
2021-07-24 15:57:04 +1000

    adjust color and background

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import classes from './Header.module.css';
 import { Fragment } from 'react';
-import image from '../../assets/headerImage.jpeg';
+import image from '../../assets/foodora.png';
 import HeaderCart from './HeaderCart';
 
 const Header = props => {
   return <Fragment>
       <header className={classes.header}>
-          <h1>Wei's Kitchen</h1>
+          <h1>Wei's Foodora</h1>
           <HeaderCart onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
